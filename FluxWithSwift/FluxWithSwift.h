274978ec1dbf2eae47c7e392f0ab18564bd3e693
g4zeru <haru25haru02@gmail.com>
2020-03-08 16:03:02 +0900

    Initial Commit

---
+++ FluxWithSwift/FluxWithSwift.h
@@ -0,0 +1,19 @@
+//
+//  FluxWithSwift.h
+//  FluxWithSwift
+//
+//  Created by iniad on 2020/03/08.
+//  Copyright © 2020 haruta-yamada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluxWithSwift.
+FOUNDATION_EXPORT double FluxWithSwiftVersionNumber;
+
+//! Project version string for FluxWithSwift.
+FOUNDATION_EXPORT const unsigned char FluxWithSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluxWithSwift/PublicHeader.h>
+
+
